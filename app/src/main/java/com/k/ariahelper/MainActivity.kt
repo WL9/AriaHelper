ef50964e57c2829585b2494d71b0423740171141
LAINE William <>
2021-10-07 11:32:14 +0200

    app structure

--- app/src/main/java/com/k/ariahelper/MainActivity.kt
+++ app/src/main/java/com/k/ariahelper/MainActivity.kt
@@ -2,10 +2,48 @@ package com.k.ariahelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.k.ariahelper.controllers.cards.CardsFragment
+import com.k.ariahelper.controllers.character.CharacterFragment
+import com.k.ariahelper.controllers.dices.DicesFragment
+import com.k.ariahelper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMain: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMain.root)
+
+        loadFragment(DicesFragment())
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        activityMain.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.dices -> {
+                    loadFragment(DicesFragment())
+                    true
+                }
+                R.id.cards -> {
+                    loadFragment(CardsFragment())
+                    true
+                }
+                R.id.character -> {
+                    loadFragment(CharacterFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
