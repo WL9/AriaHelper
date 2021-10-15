a96e99b98f4bd57b5c4833959fc3dae0d7e4bea7
LAINE William <>
2021-10-15 14:11:07 +0200

    Manage number of dices to roll

--- app/src/main/java/com/k/ariahelper/controllers/dices/DicesFragment.kt
+++ app/src/main/java/com/k/ariahelper/controllers/dices/DicesFragment.kt
@@ -1,11 +1,12 @@
 package com.k.ariahelper.controllers.dices
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.k.ariahelper.R
 import com.k.ariahelper.models.DiceModel
@@ -21,11 +22,15 @@ class DicesFragment : Fragment(), DiceClickListener {
         DiceModel("D20", R.drawable.d10, 20),
         DiceModel("D100", R.drawable.d10, 100)
     )
+    private var numberOfDices: Int = 1
 
     private lateinit var recycler: RecyclerView
     private lateinit var resultDisplay: TextView
     private lateinit var resultDetailsDisplay: TextView
     private lateinit var diceTypeDisplay: TextView
+    private lateinit var increaseDices: Button
+    private lateinit var decreaseDices: Button
+    private lateinit var dicesNumberSelection: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,6 +46,11 @@ class DicesFragment : Fragment(), DiceClickListener {
         resultDisplay = view.findViewById(R.id.resultDisplay)
         resultDetailsDisplay = view.findViewById(R.id.resultDetailsDisplay)
         diceTypeDisplay = view.findViewById(R.id.diceTypeDisplay)
+        increaseDices = view.findViewById(R.id.numberOfDicesIncrease)
+        decreaseDices = view.findViewById(R.id.numberOfDicesDecrease)
+        dicesNumberSelection = view.findViewById(R.id.numberOfDices)
+
+        setDiceNumberPicker()
 
         recycler.apply {
             layoutManager = androidx.recyclerview.widget.GridLayoutManager(activity,3)
@@ -49,11 +59,38 @@ class DicesFragment : Fragment(), DiceClickListener {
     }
 
     override fun onDiceClickListener(diceModel: DiceModel) {
-        val dicesResult = diceModel.roll(1)
+        val dicesResult = diceModel.roll(numberOfDices)
         val resultText: String = dicesResult.toString()
 
         diceTypeDisplay.text = diceModel.label
         resultDetailsDisplay.text = resultText
         resultDisplay.text = dicesResult.sum().toString()
+    }
+
+    private fun setDiceNumberPicker() {
+        updateNumberOfDicesDisplay()
+
+        increaseDices.setOnClickListener {
+            increaseDiceNumber()
+        }
+        decreaseDices.setOnClickListener {
+            decreaseDicesNumber()
+        }
+    }
+
+    private fun increaseDiceNumber() {
+        if(numberOfDices < 10)
+            numberOfDices += 1
+        updateNumberOfDicesDisplay()
+    }
+
+    private fun decreaseDicesNumber() {
+        if(numberOfDices > 1)
+            numberOfDices -= 1
+        updateNumberOfDicesDisplay()
+    }
+
+    private fun updateNumberOfDicesDisplay() {
+        dicesNumberSelection.text = numberOfDices.toString()
     }
 }
