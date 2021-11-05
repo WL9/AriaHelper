987dfeb306e09c94b24cf0493f9a4efeac29dabf
LAINE William <>
2021-11-05 12:09:23 +0100

    Create cards mechanic

---
+++ app/src/main/java/com/k/ariahelper/models/Cards.kt
@@ -0,0 +1,56 @@
+package com.k.ariahelper.models
+
+import com.k.ariahelper.R
+import kotlin.random.Random
+
+class Cards(_cards: ArrayList<Int> = arrayListOf()) {
+    private val deckSize: Int = 53
+    private val colorSize: Int = 13
+    private var cardsColors = arrayOf(
+        R.string.cardsColorSpades,
+        R.string.cardsColorDiamonds,
+        R.string.cardsColorClubs,
+        R.string.cardsColorHearts,
+        R.string.cardsColorJoker
+    )
+
+    private var cards = _cards
+
+    init {
+        if(cards.size == 0)
+        for (value in 1..deckSize)
+            cards.add(value)
+    }
+
+    fun getDeckSize(): Int {
+        return cards.size
+    }
+
+    fun draw(): Int {
+        val randomIndex = Random.Default.nextInt(cards.size)
+        val cardValue = cards[randomIndex]
+
+        cards.removeAt(randomIndex)
+        return cardValue
+    }
+
+    fun addCard(newCard: Int) {
+        cards.add(newCard)
+        cards.sort()
+    }
+
+    fun getCardValue(id: Int): String {
+        val cardTypeIndex: Int = id / colorSize
+        val cardValue: Int = id - colorSize * cardTypeIndex
+        return cardValue.toString() + " " + cardsColors[cardTypeIndex]
+    }
+
+    fun removeCard(value: Int) {
+        cards.remove(value)
+    }
+
+    fun resetDeck() {
+        cards = arrayListOf()
+        for (value in 1..deckSize) cards.add(value)
+    }
+}
